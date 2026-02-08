35f584e1e3f5975deb87302e0a2351af0ad28d8a
freedomprotocol26-ship-it <freedomprotocol26@gmail.com>
2026-02-08 17:08:43 +0000

    Mount payment routes

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
-const authRoutes = require("./routes/auth");
 const express = require("express");
 const pool = require("./db");
+
+const authRoutes = require("./routes/auth");
 const patientRoutes = require("./routes/patient");
+const subscriptionRoutes = require("./modules/subscriptions/subscription.routes");
+const paymentRoutes = require("./modules/payments/payment.routes");
 
 const app = express();
 app.use(express.json());
-app.use("/patients", patientRoutes);
+
 app.use("/auth", authRoutes);
-const subscriptionRoutes = require("./modules/subscriptions/subscription.routes");
+app.use("/patients", patientRoutes);
 app.use("/subscriptions", subscriptionRoutes);
-
-
+app.use("/payments", paymentRoutes);
 
 app.get("/health", async (req, res) => {
   try {
