f66a73e584309f4d51a72bd1f021a306d5df2ebd
freedomprotocol26-ship-it <freedomprotocol26@gmail.com>
2026-02-08 19:39:59 +0000

    Use authenticateToken middleware in facilities routes

--- src/modules/facilities/facility.routes.js
+++ src/modules/facilities/facility.routes.js
@@ -1,32 +1,21 @@
 const express = require('express');
 const router = express.Router();
 
-const authModule = require('../../middleware/auth');
+const { authenticateToken } = require('../../middleware/auth');
 const facilityController = require('./facility.controller');
 
-console.log("AUTH MODULE:", authModule);
-console.log("FACILITY CONTROLLER:", facilityController);
+router.post('/', authenticateToken, facilityController.createFacility);
 
-const authenticateJWT = authModule.authenticateJWT || authModule;
+router.post(
+  '/assign-doctor',
+  authenticateToken,
+  facilityController.assignDoctorToFacility
+);
 
-if (typeof authenticateJWT !== 'function') {
-  throw new Error('authenticateJWT is NOT a function');
-}
-
-if (typeof facilityController.createFacility !== 'function') {
-  throw new Error('createFacility is NOT a function');
-}
-
-if (typeof facilityController.assignDoctorToFacility !== 'function') {
-  throw new Error('assignDoctorToFacility is NOT a function');
-}
-
-if (typeof facilityController.getFacilityDoctors !== 'function') {
-  throw new Error('getFacilityDoctors is NOT a function');
-}
-
-router.post('/', authenticateJWT, facilityController.createFacility);
-router.post('/assign-doctor', authenticateJWT, facilityController.assignDoctorToFacility);
-router.get('/:facilityId/doctors', authenticateJWT, facilityController.getFacilityDoctors);
+router.get(
+  '/:facilityId/doctors',
+  authenticateToken,
+  facilityController.getFacilityDoctors
+);
 
 module.exports = router;
